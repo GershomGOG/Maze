9a691177108640ac4e906f358e922ae58f7c65d8
GershomGOG <GershomGOG@users.noreply.github.com>
2014-05-06 20:41:12 -0400

    Create Desc.js

---
+++ javascripts/Desc.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+/*
+These are the posible  room texts
+*/
+
+var rDesc = [	"<p>This is hell of a place for a maze.</p><p>Where to next?</p>"
+,							'<p>this other thing</p>']
