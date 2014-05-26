a11563486bc740836bef0811c053d3c106d74e27
GershomGOG <GershomGOG@users.noreply.github.com>
2014-05-25 21:31:02 -0400

    Update Desc.js

--- javascripts/Desc.js
+++ javascripts/Desc.js
@@ -19,4 +19,4 @@ var rDesc = [	"<p>This is hell of a place for a maze.</p><p>Where to next?</p>"
 
 
 
-var rDoorc = [	" maze1door"," maze2door"," maze3door"," maze4door"," maze5door"," maze6door"," maze7door"," maze8door"," maze9door"," maze10door"]
+var rDoor = [	" maze1door"," maze2door"," maze3door"," maze4door"," maze5door"," maze6door"," maze7door"," maze8door"," maze9door"," maze10door"]
