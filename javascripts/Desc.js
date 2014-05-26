7b4f408e69d33be38ad1bcfa48ed9f148b628510
GershomGOG <GershomGOG@users.noreply.github.com>
2014-05-25 21:30:18 -0400

    Update Desc.js

--- javascripts/Desc.js
+++ javascripts/Desc.js
@@ -6,3 +6,17 @@ These are the posible  room texts
 
 var rDesc = [	"<p>This is hell of a place for a maze.</p><p>Where to next?</p>"
 ,							'<p>this other thing</p>']
+
+
+
+
+
+
+
+
+
+
+
+
+
+var rDoorc = [	" maze1door"," maze2door"," maze3door"," maze4door"," maze5door"," maze6door"," maze7door"," maze8door"," maze9door"," maze10door"]
