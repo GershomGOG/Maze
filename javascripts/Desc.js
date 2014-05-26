c0984aeb4b32838d25f17d32fec665765c952db1
GershomGOG <GershomGOG@users.noreply.github.com>
2014-05-25 23:50:19 -0400

    Update Desc.js

--- javascripts/Desc.js
+++ javascripts/Desc.js
@@ -5,7 +5,7 @@ These are the posible  room texts
 */
 
 var rDesc = [	"<p>This is hell of a place for a maze.</p><p>Where to next?</p>"
-,							'<p>You see a note on the table. It reads, &ldquo;Buy Milk&rdquo;</p></p>'
+,							"<p>You see a note on the table. It reads, &ldquo;Buy Milk&rdquo;</p>"
 ,             "<p>There is a wierd smell comming from under the floorboards.</p>"
 ,             "<p>There are so many ways to get lost in here.</p>"
 ,             "<p>There must be an easier way out of here.</p>"
